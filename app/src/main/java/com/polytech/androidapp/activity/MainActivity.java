dcc848824c15117e9889d2cbdcd3e4a3a1e8d06d
Cyprien <cypconnet@wanadoo.fr>
2017-04-17 11:51:54 +0200

    -------------V19----------

--- app/src/main/java/com/polytech/androidapp/activity/MainActivity.java
+++ app/src/main/java/com/polytech/androidapp/activity/MainActivity.java
@@ -1,19 +1,35 @@
 package com.polytech.androidapp.activity;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.polytech.androidapp.R;
 
-public class MainActivity extends Activity {
+import com.facebook.FacebookSdk;
 
+public class MainActivity extends FragmentActivity {
+
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
 
         Button enter = (Button) findViewById(R.id.button2);
         enter.setOnClickListener(new View.OnClickListener() {
@@ -23,5 +39,34 @@ public class MainActivity extends Activity {
                 startActivity(myIntent);
             }
         });
+
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
