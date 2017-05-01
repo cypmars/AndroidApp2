abf67a211957707b9d44bd4d6c374bc53cdcb0a3
Cyprien <cypconnet@wanadoo.fr>
2017-05-01 17:57:13 +0200

    -------------V27----------

--- app/src/main/java/com/polytech/androidapp/activity/AdvanceSearchActivity.java
+++ app/src/main/java/com/polytech/androidapp/activity/AdvanceSearchActivity.java
@@ -1,7 +1,10 @@
 package com.polytech.androidapp.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.polytech.androidapp.R;
 
@@ -11,5 +14,20 @@ public class AdvanceSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advance_search);
+
+        setTitle(null);
+
+        Toolbar topToolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
+        topToolBar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        topToolBar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent back = new Intent(AdvanceSearchActivity.this, FirstActivity.class);
+                        startActivity(back);
+                    }
+                }
+        );
     }
 }
