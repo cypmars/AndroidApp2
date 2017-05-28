1ea792e122f71965c268c22547e26b22cd999b22
Cyprien <cypconnet@wanadoo.fr>
2017-05-28 23:06:35 +0200

    -------------V1.5.0----------

---
+++ app/src/main/java/com/polytech/androidapp/activity/SplashActivity.java
@@ -0,0 +1,65 @@
+package com.polytech.androidapp.activity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+import com.polytech.androidapp.R;
+
+import static android.view.animation.Animation.RELATIVE_TO_PARENT;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        RotateAnimation rotate = new RotateAnimation(0, 720, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(4000);
+        imageView.startAnimation(rotate);
+        startHeavyProcessing();
+    }
+
+    private void startHeavyProcessing(){
+        new LongOperation().execute("");
+    }
+
+    private class LongOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            //some heavy processing resulting in a Data String
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.interrupted();
+                }
+            }
+            return "whatever result you have";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent i = new Intent(SplashActivity.this, FirstActivity.class);
+            startActivity(i);
+            finish();
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {}
+    }
+}
